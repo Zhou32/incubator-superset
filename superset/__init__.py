508eccbe2efff289f934fe0eeaed31e16de4a6d4
Eric Dou <zhongfan.dou@zawee.work>
2019-05-09 16:22:44 +1000

    add gunicorn debug

--- superset/__init__.py
+++ superset/__init__.py
@@ -48,6 +48,12 @@ app = Flask(__name__)
 app.config.from_object(CONFIG_MODULE)
 conf = app.config
 
+########################
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.DEBUG)
+app.logger.debug('this will show in the log')
+
 #################################################################
 # Handling manifest file logic at app start
 #################################################################
