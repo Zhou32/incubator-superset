b08b920d7da0e0c66125d0de07cf949d482f12e1
LegendCode22 <phoenix22@tutanota.com>
2019-06-21 21:02:30 +0200

    Developed parameters for Admin&Owner tab page

--- superset/views/savvy_admin.py
+++ superset/views/savvy_admin.py
@@ -1,10 +1,9 @@
 from flask import g, flash, redirect, url_for
 from flask_appbuilder import BaseView
 from flask_appbuilder.views import expose
-from flask_appbuilder.security.decorators import has_access
 
 from superset import appbuilder, db
-from superset.savvy.models import Group, Site, SavvyUser, OrgRegisterUser, Organization
+from superset.savvy.models import Group, SavvyUser, OrgRegisterUser, Organization
 from superset.savvy.decorator import has_access_savvybi_admin
 
 def get_organization_name():
@@ -46,18 +45,36 @@ class AccountView(SavvybiAdminView):
             flash('Access is denied. Only organization owner can access it.', 'info')
             return redirect(appbuilder.get_url_for_index)
 
-        user = db.session.query(SavvyUser).filter_by(id=g.user.get_id()).first()
+        overview_values = {} # required parameters for Overview tab page in Account/About
+        admin_owner_list = []  # required parameters for Admins&Owners tab page in Account/About
+        bav_values = {}  # required parameters for BAV tab page in Account/About
+        role_matches = {'org_owner': 'Owner', 'org_superuser': 'Super User', 'org_user': 'Standard User', 'org_viewer': 'Viewer', 'Admin': 'Admin'}
 
+        user = db.session.query(SavvyUser).filter_by(id=g.user.get_id()).first()
         if user.email_confirm is True:
             organization = db.session.query(Organization).filter(Organization.users.any(id=user.id)).scalar()
-            org_name = organization.organization_name
+            overview_values['organization'] = organization.organization_name
+            overview_values['date_created'] = organization.date_created.date()
+            overview_values['meter_count'] = len(organization.sites)
+
+            for user in organization.users:
+                individual = {}
+                individual['fullname'] = "{} {}".format(user.first_name.capitalize(), user.last_name.capitalize())
+                individual['email'] = user.email
+                individual['role'] = role_matches[user.roles[0].name]
+                admin_owner_list.append(individual)
         else:
             org_register = db.session.query(OrgRegisterUser).filter_by(email=user.email).first()
-            org_name = org_register.organization
+            overview_values['organization'] = org_register.organization
+            overview_values['date_created'] = org_register.registration_date.date()
+            overview_values['meter_count'] = 0
+            admin_owner_list['users'] = []
+
         username = '{} {}'.format(user.first_name, user.last_name)
         return self.render_template(
             'savvy/account/about.html',
-            organization=org_name.capitalize(),
+            overview=overview_values,
+            admin_owner=admin_owner_list,
             username=username
         )
 
