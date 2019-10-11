84f4d2f3cb072277425f6efe097d71d3c0a4f879
周子豪 <zihao@Hills-MacBook.local>
2019-10-11 15:25:35 +1100

    Create requirement.sh

---
+++ scripts/requirement.sh
@@ -0,0 +1,21 @@
+upgrade_db=$1
+init_ss=$2
+
+cd ~/incubator-superset
+
+. ~/incubator-superset/venv/bin/activate
+python --version
+
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+pip install -e .
+
+if [ $upgrade_db = "true" ]
+then
+  superset db upgrade
+fi
+
+if [ $init_ss = "true" ]
+then
+  superset init
+fi
