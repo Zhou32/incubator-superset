1e0efdaa48f32dc2180724aa1a9db3be0b919d05
周子豪 <zihao@Hills-MacBook.local>
2019-10-18 10:59:50 +1100

    Update run-backend.sh

--- scripts/run-backend.sh
+++ scripts/run-backend.sh
@@ -2,8 +2,8 @@ sudo systemctl enable redis-server.service
 
 cd ~/incubator-superset
 . ~/incubator-superset/venv/bin/activate
-export SMTP_USERNAME = $1
-export SMTP_PASSWORD = $2
+export SMTP_USERNAME=$1
+export SMTP_PASSWORD=$2
 export FLASK_ENV=development
 nohup superset run -p 8088 --host 0.0.0.0 --with-threads --reload --debugger >> logs.txt 2>&1 &
 ps -aux
