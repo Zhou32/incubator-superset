33ab3cea222b063ff795382aa559d6398a317733
周子豪 <zihao@Hills-MacBook.local>
2019-10-18 15:32:18 +1100

    Update run-backend.sh

--- scripts/run-backend.sh
+++ scripts/run-backend.sh
@@ -4,6 +4,9 @@ cd ~/incubator-superset
 . ~/incubator-superset/venv/bin/activate
 export SMTP_USERNAME=$1
 export SMTP_PASSWORD=$2
+export STRIPE_SK=$3
+export STRIPE_PK=$4
+
 export FLASK_ENV=development
 nohup superset run -p 8088 --host 0.0.0.0 --with-threads --reload --debugger >> logs.txt 2>&1 &
 ps -aux
