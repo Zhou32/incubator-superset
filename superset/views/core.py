1c0e380a484e19a7949f0bb1bf12bc394f80602b
Eric Dou <zhongfan.dou@zawee.work>
2019-10-18 10:54:56 +1000

    Modified upgrade logic. Fetch finalised invoices
    Upgrade or renew plan after receive payment succeeded. Or if paid before, upgrade immediately.

--- superset/views/core.py
+++ superset/views/core.py
@@ -781,6 +781,10 @@ class SolarBIBillingView(ModelView):
         cus_email = cus_obj.email
         cus_address = cus_obj.address
 
+        cus_invoices = stripe.Invoice.list(customer=cus_obj['id'])
+        cus_invoices = list( {'invoice_id':invoice['id'], 'date':datetime.utcfromtimestamp(invoice['created']),
+                              'link': invoice['invoice_pdf']} for invoice in cus_invoices)
+
         entry_point = 'billing'
         payload = {
             'user': bootstrap_user_data(g.user),
@@ -789,6 +793,7 @@ class SolarBIBillingView(ModelView):
             'cus_info': {'cus_name': cus_name, 'cus_email': cus_email, 'cus_address': cus_address},
             'pm_id': team.stripe_pm_id,
             'plan_id': plan.stripe_id,
+            'invoice_list':cus_invoices,
         }
 
         return self.render_template(
@@ -858,10 +863,19 @@ class SolarBIBillingView(ModelView):
                 logging.info(f'Upgrading team {team.team_name} from {old_plan.stripe_id} to {new_plan.stripe_id}')
                 old_count = team_sub.remain_count
 
-                #TODO this line needs reconsider
-                team_sub.remain_count = old_count + new_plan.num_request - old_plan.num_request
+                # If upgrading from paid plan to higher paid plan, cannot trigger payment succeeded event as it will be
+                # on next billing cycle. Since the customer has successfully paid before, assume the card is still valid,
+                # upgrade plan immediately
+                if old_plan.id != 1:
+                    logging.info('Customer paid before, upgrade immediately, prorate to next billing cycle')
+                    team_sub.remain_count = old_count + new_plan.num_request - old_plan.num_request
+                    team_sub.plan = new_plan.id
 
-                team_sub.plan = new_plan.id
+                # Otherwise if upgrade from free to paid, then will trigger payment event, wait for event and let webhook
+                # upgrade the plan
+                else:
+                    logging.info('Customer new to paid plan, will upgrade after payment succeeded')
+
 
                 sub_list = stripe.Subscription.list(customer=team.stripe_user_id)
                 if len(sub_list['data']) ==2:
@@ -905,17 +919,36 @@ class SolarBIBillingView(ModelView):
 
     #TODO handle invoice.payment_succeeded event, update remain counts accordingly
     def renew(self, event_object):
-        customer = stripe.Customer.retrieve(event_object['customer'])
         paid_list = event_object['lines']['data']
 
+        # Check lines on the paid list
+        if len(paid_list) > 1:
+            logging.info('Contains more than one items in the invoice')
+            for item in paid_list:
+                logging.info(f'Customer {event_object["customer_name"]} Item {item["plan"]["nickname"]}')
 
-        pass
+        try:
+            stripe_plan = paid_list[0]
+            local_plan = self.appbuilder.get_session.query(Plan).filter_by(stripe_id=stripe_plan["plan"]['id'])
+
+            # Fetch team_subscription by the subscription on invoice
+            team_sub = self.appbuilder.get_session.query(TeamSubscription).filter_by(stripe_sub_id=stripe_plan['subscription'])
+
+            # Set subscribed plan to the plan on the invoice, and reset remain count
+            # This event should happen only when customer upgrade to paid from free for the first time and paid upfront,
+            # or at the start of new billing cycle. So it is safe to reset.
+            team_sub.plan = local_plan.id
+            team_sub.remain_count = local_plan.num_search
+
+            self.appbuilder.get_session.commit()
+        except Exception as e:
+            self.appbuilder.get_session.rollback()
+            logging.error(e)
 
     @api
     @expose('/webhook', methods=['POST'])
     def webhook(self):
         body = request.data
-        event = None
         try:
             event = stripe.Event.construct_from(
                 json.loads(body), stripe.api_key
@@ -924,8 +957,10 @@ class SolarBIBillingView(ModelView):
             logging.warning(e)
             return Response(status=400)
         print(event.type)
+
+        # TODO need to log events by id, and request id
         if event.type == 'invoice.payment_succeeded':
-            print(event)
+            # print(event)
             self.renew(event['data']['object'])
         return Response(status=200)
 
