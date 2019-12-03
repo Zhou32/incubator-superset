3c0fed4af03099d751cc4b765c0987f697dc7a6e
Colin Wang <chenyangw233@gmail.com>
2019-12-03 14:29:39 +1100

    Remove ab_test temporarily

--- superset/__init__.py
+++ superset/__init__.py
@@ -29,7 +29,7 @@ from flask_compress import Compress
 from flask_migrate import Migrate
 from flask_talisman import Talisman
 from flask_wtf.csrf import CSRFProtect
-from flask_split import split
+# from flask_split import split
 from werkzeug.contrib.fixers import ProxyFix
 import wtforms_json
 
