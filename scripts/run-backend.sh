fb5165da11bc98c4da4af18c26ebac8bf12141a5
Colin Wang <chenyangw233@gmail.com>
2019-12-13 14:52:28 +1100

    Add MC API to run-backend.sh

--- scripts/run-backend.sh
+++ scripts/run-backend.sh
@@ -6,5 +6,6 @@ export SMTP_USERNAME=$1
 export SMTP_PASSWORD=$2
 export STRIPE_SK=$3
 export STRIPE_PK=$4
+export MC_API_KEY=$5
 
 celery worker --app=superset.sql_lab:celery_app --pool=gevent -Ofair & gunicorn --bind  0.0.0.0:8088 --workers $((2 * $(getconf _NPROCESSORS_ONLN) + 1))  --timeout 60 --limit-request-line 0 --limit-request-field_size 0 superset:app --log-level info --log-file logs.txt --capture-output
